0be999044d38b464913858faa5912328ff920385
Curry Cunningham <cunninghamcurry@gmail.com>
2020-03-21 19:16:23 -0800

    Init script to run separate model with PDO and NPGO as covariates

---
+++ Fit-DLM-Region-Separate.R
@@ -0,0 +1,16 @@
+#==================================================================================================
+#Project Name: COAST-WIDE SALMON RECRUITMENT ANALYSIS - Fit DLM-Ricker with Time-varying Components Shared Among Regions
+#Creator: Curry James Cunningham, NOAA/NMFS, ABL
+#Date: 8.20.18
+#
+#Purpose: To fit a dynamic linear Ricker model with 
+#
+#
+#==================================================================================================
+#NOTES:
+#  a) Separte models for PDO and NPGO to avoid confounding due to change in correlation structure
+# 
+# TIMINGS: 
+
+
+#==================================================================================================
